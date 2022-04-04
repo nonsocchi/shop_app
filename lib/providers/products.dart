ced3abce7f87ec12fda0b8a5ba739c11c280003a
devnons <unimkeadie@gmail.com>
2022-04-04 12:18:07 +0100

    Added ability to update products via patch request.

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -98,8 +98,21 @@ class Products with ChangeNotifier {
     }
   }
 
-  void updateProduct(String? id, Product newProduct) {
+  Future<void> updateProduct(String? id, Product newProduct) async {
     final prodIndex = _items.indexWhere((product) => product.id == id);
+    if (prodIndex >= 0) {
+      final String url =
+          'https://shop-app-e7566-default-rtdb.firebaseio.com/products/$id.json';
+      await http.patch(
+        Uri.parse(url),
+        body: json.encode({
+          'title': newProduct.title,
+          'description': newProduct.description,
+          'imageUrl': newProduct.imageUrl,
+          'price': newProduct.price,
+        }),
+      );
+    }
     _items[prodIndex] = newProduct;
     notifyListeners();
   }
--- lib/routes/edit_product_screen.dart
+++ lib/routes/edit_product_screen.dart
@@ -90,17 +90,15 @@ class _EditProductScreenState extends State<EditProductScreen> {
       _isLoading = true;
     });
     if (_editedProduct.id != null) {
-      Provider.of<Products>(context, listen: false)
+      await Provider.of<Products>(context, listen: false)
           .updateProduct(_editedProduct.id, _editedProduct);
-      setState(() {
-        _isLoading = false;
-      });
       Navigator.of(context).pop();
     } else {
       try {
         await Provider.of<Products>(context, listen: false)
             .addProduct(_editedProduct);
       } catch (error) {
+        // ignore: prefer_void_to_null
         return showDialog<Null>(
           context: context,
           builder: (ctx) => AlertDialog(
