8f1817ba9df9aa2f95edf96e0e90a54fb0686d95
devnons <unimkeadie@gmail.com>
2022-04-21 10:59:28 +0100

    Added autologout for users after Firebase token expires.

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
@@ -8,6 +9,7 @@ class Auth extends ChangeNotifier {
   String? _token;
   DateTime? _expiryDate;
   String? _userId;
+  Timer? _authTimer;
 
   bool get isAuth => token != null;
 
@@ -45,6 +47,8 @@ class Auth extends ChangeNotifier {
       _expiryDate = DateTime.now()
           .add(Duration(seconds: int.parse(responseData['expiresIn'])));
 
+      _autoLogout();
+
       notifyListeners();
     } catch (error) {
       rethrow;
@@ -63,6 +67,19 @@ class Auth extends ChangeNotifier {
     _token = null;
     _expiryDate = null;
     _userId = null;
+
+    if (_authTimer != null) {
+      _authTimer!.cancel();
+      _authTimer = null;
+    }
+
     notifyListeners();
+  }
+
+  void _autoLogout() {
+    if (_authTimer != null) _authTimer!.cancel();
+
+    final timeToExpiry = _expiryDate!.difference(DateTime.now()).inSeconds;
+    _authTimer = Timer(Duration(seconds: timeToExpiry), logout);
   }
 }
